fa34e531f29d70bba5491846c66976052f4d12bc
FranciscoJavierJimenezMontes0101 <FranJJM@uma.es>
2021-05-29 08:19:39 +0200

    pues nada aqui sigo trabajando, en fin sigamos

---
+++ BooKy/app/src/main/java/com/example/booky/ComentarioVerActivity.java
@@ -0,0 +1,14 @@
+package com.example.booky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComentarioVerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comentario_ver);
+    }
+}
