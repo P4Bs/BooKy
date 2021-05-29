34569e4615bff86a83791a2a5f9b4bb3e54eefca
FranciscoJavierJimenezMontes0101 <FranJJM@uma.es>
2021-05-29 06:05:49 +0200

    pues nada aqui sigo trabajando, en fin sigamos

--- BooKy/app/src/main/java/com/example/booky/CartaAdminActivity.java
+++ BooKy/app/src/main/java/com/example/booky/CartaAdminActivity.java
@@ -3,12 +3,29 @@ package com.example.booky;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class CartaAdminActivity extends AppCompatActivity {
+
+    ListView lv_platoslist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carta_admin);
+
+        lv_platoslist = findViewById(R.id.lv_platoslist);
+
+        DatabaseHelper baseDeDatos = new DatabaseHelper(CartaAdminActivity.this);
+        List<Plato> todos_platos = baseDeDatos.get_lista_platos();
+
+        ArrayAdapter platosArray = new ArrayAdapter<Plato>(CartaAdminActivity.this, android.R.layout.simple_list_item_1, todos_platos);
+        //lv_Plat
+
+        Toast.makeText(CartaAdminActivity.this,todos_platos.toString(),Toast.LENGTH_SHORT).show();
     }
 }
